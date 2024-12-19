909e0d4d27c52c37f829fe9a0229237a17ff8216
valeStup <valentin.stupp@gmx.de>
2024-12-19 21:36:22 +0100

    delete funct (for selector)
    added the delete function for selected laps, got rid of edit function for selector due to lack of need

--- script.js
+++ script.js
@@ -70,7 +70,6 @@ function getFileName(name) {
 function changeLapWidths() {
     for (let i = 0; i < blockLaps.length; i++) {
         let lapNum = blockLaps[i].name;
-        console.log(lapNum);
         let blockLap = document.querySelector(`.blockLapDiv-${lapNum}`);
         if (blockLaps[i].dockStatus = "docked") {
             const blockWidth = ((blockLaps[i].duration) / decMinsTotal) * 1000 ;
@@ -796,8 +795,6 @@ document.addEventListener('DOMContentLoaded', () => {
     // Update rectangle size and check for overlaps
     document.addEventListener('mousemove', (event) => {
       if (!isSelecting) return;
-      console.log("mousemove");
-  
       const currentX = event.pageX;
       const currentY = event.pageY;
   
@@ -823,10 +820,8 @@ document.addEventListener('DOMContentLoaded', () => {
           rectTop + rectHeight < elementRect.top
         );
         if (overlap) {
-            console.log("found one");
             element.classList.add('selected');
             selectedElements.push({name: element.ariaLabel});
-            console.log(selectedElements);
             selectingrn = true ;
             openGroupLapEditor();
         }
@@ -861,7 +856,6 @@ document.addEventListener('DOMContentLoaded', () => {
         groupLapEditor.classList.add("grpEdit");
         groupLapEditor.innerHTML = `
         <div class="blockGrp-svgs">
-            <svg id="pencil-svg-blockGrp" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.7.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path id="pencil-svg-blockGrp" d="M410.3 231l11.3-11.3-33.9-33.9-62.1-62.1L291.7 89.8l-11.3 11.3-22.6 22.6L58.6 322.9c-10.4 10.4-18 23.3-22.2 37.4L1 480.7c-2.5 8.4-.2 17.5 6.1 23.7s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L387.7 253.7 410.3 231zM160 399.4l-9.1 22.7c-4 3.1-8.5 5.4-13.3 6.9L59.4 452l23-78.1c1.4-4.9 3.8-9.4 6.9-13.3l22.7-9.1 0 32c0 8.8 7.2 16 16 16l32 0zM362.7 18.7L348.3 33.2 325.7 55.8 314.3 67.1l33.9 33.9 62.1 62.1 33.9 33.9 11.3-11.3 22.6-22.6 14.5-14.5c25-25 25-65.5 0-90.5L453.3 18.7c-25-25-65.5-25-90.5 0zm-47.4 168l-144 144c-6.2 6.2-16.4 6.2-22.6 0s-6.2-16.4 0-22.6l144-144c6.2-6.2 16.4-6.2 22.6 0s6.2 16.4 0 22.6z"/></svg>
             <svg id="bin-svg-blockGrp" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.7.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path id="bin-svg-blockGrp" d="M135.2 17.7L128 32 32 32C14.3 32 0 46.3 0 64S14.3 96 32 96l384 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-96 0-7.2-14.3C307.4 6.8 296.3 0 284.2 0L163.8 0c-12.1 0-23.2 6.8-28.6 17.7zM416 128L32 128 53.2 467c1.6 25.3 22.6 45 47.9 45l245.8 0c25.3 0 46.3-19.7 47.9-45L416 128z"/></svg>
             <svg id="copy-svg-blockGrp" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path id="copy-svg-blockGrp" d="M208 0L332.1 0c12.7 0 24.9 5.1 33.9 14.1l67.9 67.9c9 9 14.1 21.2 14.1 33.9L448 336c0 26.5-21.5 48-48 48l-192 0c-26.5 0-48-21.5-48-48l0-288c0-26.5 21.5-48 48-48zM48 128l80 0 0 64-64 0 0 256 192 0 0-32 64 0 0 48c0 26.5-21.5 48-48 48L48 512c-26.5 0-48-21.5-48-48L0 176c0-26.5 21.5-48 48-48z"/></svg>
         </div>
@@ -889,19 +883,33 @@ document.addEventListener('DOMContentLoaded', () => {
     selectingrn = false ;
     }
     
-  });
-
+    
 document.addEventListener("click", function(e) {
-    if (e.target.id === 'pencil-svg-blockGrp') {
-        editBlockLaps();
+    if (e.target.id === 'bin-svg-blockGrp') {
+        deleteBlockLaps();
     }
 })
 
-function editBlockLaps() {
+function deleteBlockLaps() {
     let selectedLaps = [];
     document.querySelectorAll(".focused").forEach((element) => {
         selectedLaps.push({name: element.ariaLabel});
     })
     selectedLaps = selectedLaps.filter((lap) => lap.name !== null);
     console.log(selectedLaps);
+
+
+    selectedLaps.forEach((lap) => {
+        const lapIndex = lap.name - 1 ;
+        const lapProps = document.querySelector(`.blockLapDiv-${lap.name}`);
+        decMinsTotal -= blockLaps[lapIndex].duration ;
+        blockLaps[lapIndex].duration = 0;
+        changeLapWidths();
+        changeLapArrangements();
+        lapProps.style.display = "none" ;
+    })
+    closeGroupLapEditor();
 }
+
+
+});
